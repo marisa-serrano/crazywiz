8645be22b5ff37be55a9f95491dd93fd576f6f5b
Marisa Serrano <marisa.a.serrano@gmail.com>
2023-02-14 14:33:58 +0000

    Add simplegfx library

--- src/org/academiadecodigo/wizards/Main.java
+++ src/org/academiadecodigo/wizards/Main.java
@@ -1,7 +1,10 @@
 package org.academiadecodigo.wizards;
 
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-
+        Rectangle rect = new Rectangle();
+        rect.draw();
     }
 }
---
+++ src/org/academiadecodigo/wizards/objects/Wizard.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.wizards.objects;
+
+public class Wizard {
+}
